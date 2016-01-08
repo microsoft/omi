1e29ad94fa8f6c70327d64afa56b5f1d8261b903
Paul Allen <paulall@microsoft.com>
2016-01-08 12:22:18 -0800

    remove obsolete build files

--- Unix/project.mk
+++
@@ -1,4 +0,0 @@
-!include $(BASEDIR)\Admin\project.mk
-
-#The below command switches the default OACR project from admin to the custom OACR project mentioned below. Commenting the below line will set the OACR project back to default
-OACR_RULES=WINOMI
