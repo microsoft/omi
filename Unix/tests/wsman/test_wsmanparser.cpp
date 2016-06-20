d0c173c39d2da8c1def67dd0525906aa97bff05e
George Fleming <v-geflem@microsoft.com>
2016-06-20 09:53:22 -0700

    Debug #2

--- Unix/tests/wsman/test_wsmanparser.cpp
+++ Unix/tests/wsman/test_wsmanparser.cpp
@@ -43,15 +43,18 @@ NitsEndCleanup
 
 NitsTestWithSetup(TestGetResponse, TestParserSetup)
 {
-    XML *xml = (XML *) PAL_Malloc(sizeof(XML)); if(!TEST_ASSERT(xml != NULL)) NitsReturn;
     MI_Uint32 count = 0;
     MI_Value value;
     MI_Type type;
     const MI_Char *name;
     MI_Uint32 flags;
-    MI_Uint32 i;
+    WSMAN_WSHeader wsheaders;
+    XML xml;
+    MI_Instance *instance = NULL;
+    Batch *batch = NULL;
+//    PostInstanceMsg *msg = NULL;
 
-    /* Sample GetResponse from https://msdn.microsoft.com/en-us/library/cc251505.aspx */
+    // Sample GetResponse from https://msdn.microsoft.com/en-us/library/cc251505.aspx
     XML_Char data[] = PAL_T("<s:Envelope xml:lang=\"en-US\" ")
         PAL_T("xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\" ")
         PAL_T("xmlns:a=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" ") 
@@ -82,90 +85,149 @@ NitsTestWithSetup(TestGetResponse, TestParserSetup)
         PAL_T("</s:Body>")
         PAL_T("</s:Envelope>");
 
-    WSMAN_WSHeader wsheaders;
     memset(&wsheaders, 0, sizeof(wsheaders));
 
-    PostInstanceMsg *msg = PostInstanceMsg_New(0);
-    Instance_NewDynamic(&msg->instance, MI_T("data"), MI_FLAG_CLASS, msg->base.batch);
+    XML_Init(&xml);
 
-    printf("0");
-
-    XML_Init(xml);
-
-    XML_RegisterNameSpace(xml, 's',
+    XML_RegisterNameSpace(&xml, 's',
                           ZT("http://www.w3.org/2003/05/soap-envelope"));
     
-    XML_RegisterNameSpace(xml, 'a',
+    XML_RegisterNameSpace(&xml, 'a',
                           ZT("http://schemas.xmlsoap.org/ws/2004/08/addressing"));
 
-    XML_RegisterNameSpace(xml, 'w',
+    XML_RegisterNameSpace(&xml, 'w',
                           ZT("http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd"));
 
+    XML_SetText(&xml, data);
 
-    XML_SetText(xml, data);
+//    if (NitsAssert((msg = PostInstanceMsg_New(0)), PAL_T("Unable to post new instance")))
+//    {
+//        goto cleanup;
+//    }
+//    if (!NitsCompare(MI_RESULT_OK, Instance_NewDynamic(&instance, MI_T("test"), MI_FLAG_CLASS, batch), PAL_T("Unable to create new instance")))
+//    {
+//        goto cleanup;
+//    }
 
-    printf("1\n");
-
-    UT_ASSERT(WS_ParseSoapEnvelope(xml) == 0);
-    UT_ASSERT(xml->status == 0);
-
-    UT_ASSERT(WS_ParseWSHeader(xml, &wsheaders, USERAGENT_UNKNOWN) == 0);
-    UT_ASSERT(xml->status == 0);
-
-    UT_ASSERT(wsheaders.foundAction);
-    UT_ASSERT(wsheaders.rqtAction == WSMANTAG_ACTION_GET_RESPONSE);
-    UT_ASSERT(Tcscmp(wsheaders.rqtMessageID, PAL_T("uuid:2DAB718A-0103-4E0A-AB17-06C8A5530D2B")) == 0);
-    UT_ASSERT(Tcscmp(wsheaders.rspRelatesTo, PAL_T("uuid:5E6FD101-710A-4EEA-A50D-70C0BF863AA3")) == 0);
-
-    UT_ASSERT(WS_ParseGetResponseBody(xml, msg->base.batch, &msg->instance) == 0);
-    UT_ASSERT(xml->status == 0);
-
-    printf("2\n");
-
-    UT_ASSERT(__MI_Instance_GetElementCount(msg->instance, &count) == 0);
-    UT_ASSERT(count == 3);    
-    
-    for (i=0; i<count; i++)
+    if (!NitsCompare(MI_RESULT_OK, WS_ParseSoapEnvelope(&xml), PAL_T("Parse soap envelope error")))
     {
-        printf("%d\n", i+3);
-
-        UT_ASSERT(__MI_Instance_GetElementAt(msg->instance, i, &name, &value, &type, &flags) == 0);
-        switch(i)
-        {
-          case 0:
-            UT_ASSERT(Tcscmp(name, PAL_T("Data1")) == 0);
-            UT_ASSERT(type == MI_STRING);
-            UT_ASSERT(Tcscmp((MI_String)value.string, PAL_T("Hello World")) == 0);
-            break;
-          case 1:
-            UT_ASSERT(Tcscmp(name, PAL_T("id")) == 0);
-            UT_ASSERT(type == MI_STRING);
-            UT_ASSERT(Tcscmp((MI_String)value.string, PAL_T("1")) == 0);
-            break;
-          case 2:
-            UT_ASSERT((flags & MI_FLAG_NULL) == MI_FLAG_NULL);
-            break;
-        }
+        goto cleanup;
     }
 
-    printf("6\n");
+    if (!NitsCompare(MI_RESULT_OK, WS_ParseWSHeader(&xml, &wsheaders, USERAGENT_UNKNOWN), PAL_T("Parse header error")))
+    {
+        goto cleanup;
+    }
 
-    PAL_Free(xml);
-    PostInstanceMsg_Release(msg);
+    if (!NitsCompare(MI_TRUE, wsheaders.foundAction, PAL_T("No action in header")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompare(wsheaders.rqtAction, WSMANTAG_ACTION_GET_RESPONSE, PAL_T("Action is not Get Response")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompareString(wsheaders.rqtMessageID, PAL_T("uuid:2DAB718A-0103-4E0A-AB17-06C8A5530D2B"), PAL_T("Message ID error")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompareString(wsheaders.rspRelatesTo, PAL_T("uuid:5E6FD101-710A-4EEA-A50D-70C0BF863AA3"), PAL_T("RelatesTo error")))
+    {
+        goto cleanup;
+    }
+
+    if (NitsAssert((batch = Batch_New(BATCH_MAX_PAGES)), PAL_T("Unable to create new batch")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompare(MI_RESULT_OK, WS_ParseInstanceBody(&xml, batch, &instance), PAL_T("Unable to retrieve instance")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompare(MI_RESULT_OK, xml.status, PAL_T("Retrieve instance returned error xml status")))
+    {
+        goto cleanup;
+    }
+
+    if (!NitsCompare(MI_RESULT_OK, __MI_Instance_GetElementCount(instance, &count), PAL_T("Unable to get element count")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompare(count, 3, PAL_T("Element count error")))
+    {
+        goto cleanup;
+    }
+    
+    if (!NitsCompare(MI_RESULT_OK, __MI_Instance_GetElementAt(instance, 0, &name, &value, &type, &flags), PAL_T("Unable to get element 1")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompareString(name, PAL_T("Data1"), PAL_T("Invalid element #1 name")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompare(type, MI_STRING, PAL_T("Invalid element #1 type")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompareString(value.string, PAL_T("Hello World"), PAL_T("Invalid element #1 value")))
+    {
+        goto cleanup;
+    }
+
+    if (!NitsCompare(MI_RESULT_OK, __MI_Instance_GetElementAt(instance, 1, &name, &value, &type, &flags), PAL_T("Unable to get element 2")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompareString(name, PAL_T("id"), PAL_T("Invalid element #2 name")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompare(type, MI_STRING, PAL_T("Invalid element #2 type")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompareString(value.string, PAL_T("1"), PAL_T("Invalid element #2 value")))
+    {
+        goto cleanup;
+    }
+
+    if (!NitsCompare(MI_RESULT_OK, __MI_Instance_GetElementAt(instance, 2, &name, &value, &type, &flags), PAL_T("Unable to get element 3")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompare((flags & MI_FLAG_NULL), MI_FLAG_NULL, PAL_T("Invalid element #3 flags")))
+    {
+        goto cleanup;
+    }
+
+    cleanup:
+    if (batch)
+    {
+        Batch_Delete(batch);
+    }
+//    if (msg)
+//    {
+//        PostInstanceMsg_Release(msg);
+//    }
 }
 NitsEndTest
 
+/*
 NitsTestWithSetup(TestGetResponse2, TestParserSetup)
 {
-    XML *xml = (XML *) PAL_Malloc(sizeof(XML)); if(!TEST_ASSERT(xml != NULL)) NitsReturn;
     MI_Uint32 count = 0;
     MI_Value value;
     MI_Type type;
     const MI_Char *name;
     MI_Uint32 flags;
     MI_Uint32 i;
+    Batch *batch = Batch_New(INFINITE);
+    WSMAN_WSHeader wsheaders;
+    MI_Instance *instance = NULL;
+    XML xml;
 
-    /* Sample GetResponse from https://msdn.microsoft.com/en-us/library/cc251505.aspx, but with added newline chars */
+    // Sample GetResponse from https://msdn.microsoft.com/en-us/library/cc251505.aspx, but with added newline chars
     XML_Char data[] = PAL_T("<s:Envelope xml:lang=\"en-US\" ")
         PAL_T("xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\" ")
         PAL_T("xmlns:a=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" ") 
@@ -196,46 +258,43 @@ NitsTestWithSetup(TestGetResponse2, TestParserSetup)
         PAL_T("</s:Body>\n")
         PAL_T("</s:Envelope>\n");
 
-    WSMAN_WSHeader wsheaders;
     memset(&wsheaders, 0, sizeof(wsheaders));
 
-    PostInstanceMsg *msg = PostInstanceMsg_New(0);
-    Instance_NewDynamic(&msg->instance, MI_T("data"), MI_FLAG_CLASS, msg->base.batch);
+    XML_Init(&xml);
 
-    XML_Init(xml);
-
-    XML_RegisterNameSpace(xml, 's',
+    XML_RegisterNameSpace(&xml, 's',
                           ZT("http://www.w3.org/2003/05/soap-envelope"));
     
-    XML_RegisterNameSpace(xml, 'a',
+    XML_RegisterNameSpace(&xml, 'a',
                           ZT("http://schemas.xmlsoap.org/ws/2004/08/addressing"));
 
-    XML_RegisterNameSpace(xml, 'w',
+    XML_RegisterNameSpace(&xml, 'w',
                           ZT("http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd"));
 
+    XML_SetText(&xml, data);
 
-    XML_SetText(xml, data);
+    if (!NitsCompare(MI_RESULT_OK, Instance_NewDynamic(&instance, MI_T("test"), MI_FLAG_CLASS, batch) == 0);
 
-    UT_ASSERT(WS_ParseSoapEnvelope(xml) == 0);
-    UT_ASSERT(xml->status == 0);
+    if (!NitsCompare(MI_RESULT_OK, WS_ParseSoapEnvelope(&xml) == 0);
+    if (!NitsCompare(MI_RESULT_OK, xml.status == 0);
 
-    UT_ASSERT(WS_ParseWSHeader(xml, &wsheaders, USERAGENT_UNKNOWN) == 0);
-    UT_ASSERT(xml->status == 0);
+    if (!NitsCompare(MI_RESULT_OK, WS_ParseWSHeader(&xml, &wsheaders, USERAGENT_UNKNOWN) == 0);
+    if (!NitsCompare(MI_RESULT_OK, xml.status == 0);
 
-    UT_ASSERT(wsheaders.foundAction);
-    UT_ASSERT(wsheaders.rqtAction == WSMANTAG_ACTION_GET_RESPONSE);
-    UT_ASSERT(Tcscmp(wsheaders.rqtMessageID, PAL_T("uuid:2DAB718A-0103-4E0A-AB17-06C8A5530D2B")) == 0);
-    UT_ASSERT(Tcscmp(wsheaders.rspRelatesTo, PAL_T("uuid:5E6FD101-710A-4EEA-A50D-70C0BF863AA3")) == 0);
+    if (!NitsCompare(MI_RESULT_OK, wsheaders.foundAction);
+    if (!NitsCompare(MI_RESULT_OK, wsheaders.rqtAction == WSMANTAG_ACTION_GET_RESPONSE);
+    if (!NitsCompare(MI_RESULT_OK, Tcscmp(wsheaders.rqtMessageID, PAL_T("uuid:2DAB718A-0103-4E0A-AB17-06C8A5530D2B")) == 0);
+    if (!NitsCompare(MI_RESULT_OK, Tcscmp(wsheaders.rspRelatesTo, PAL_T("uuid:5E6FD101-710A-4EEA-A50D-70C0BF863AA3")) == 0);
 
-    UT_ASSERT(WS_ParseGetResponseBody(xml, msg->base.batch, &msg->instance) == 0);
-    UT_ASSERT(xml->status == 0);
+    if (!NitsCompare(MI_RESULT_OK, WS_ParseInstanceBody(&xml, batch, &instance) == 0);
+    if (!NitsCompare(MI_RESULT_OK, xml.status == 0);
 
-    UT_ASSERT(__MI_Instance_GetElementCount(msg->instance, &count) == 0);
-    UT_ASSERT(count == 3);    
+    if (!NitsCompare(MI_RESULT_OK, __MI_Instance_GetElementCount(instance, &count) == 0);
+    if (!NitsCompare(MI_RESULT_OK, count == 3);    
     
     for (i=0; i<count; i++)
     {
-        UT_ASSERT(__MI_Instance_GetElementAt(msg->instance, i, &name, &value, &type, &flags) == 0);
+        UT_ASSERT(__MI_Instance_GetElementAt(instance, i, &name, &value, &type, &flags) == 0);
         switch(i)
         {
           case 0:
@@ -254,7 +313,15 @@ NitsTestWithSetup(TestGetResponse2, TestParserSetup)
         }
     }
 
-    PAL_Free(xml);
-    PostInstanceMsg_Release(msg);
+    if (instance)
+    {
+        __MI_Instance_Delete(instance);
+    }
+    if (batch)
+    {
+        Batch_Delete(batch);
+    }
 }
 NitsEndTest
+
+*/
