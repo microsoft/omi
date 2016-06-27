7c574dbb036097939a47f720e3b640d34f466912
George Fleming <v-geflem@microsoft.com>
2016-06-27 16:08:25 -0700

    Added fault test

--- Unix/tests/wsman/test_wsmanparser.cpp
+++ Unix/tests/wsman/test_wsmanparser.cpp
@@ -31,6 +31,7 @@
 #include <pal/strings.h>
 #include <wsman/wstags.h>
 #include <wsman/wsmanparser.h>
+#include <wsman/wsbuf.h>
 
 
 using namespace std;
@@ -359,3 +360,130 @@ NitsTestWithSetup(TestGetResponse2, TestParserSetup)
 }
 NitsEndTest
 
+NitsTestWithSetup(TestFaultResponse, TestParserSetup)
+{
+    WSMAN_WSHeader wsheaders;
+    XML xml;
+    WSMAN_WSFault fault;
+    MI_Result resultCode;
+    WSBUF_FAULT_CODE faultCode;
+
+    // Sample FaultResponse from command "omicli ci -u u -p p --auth Basic --hostname localhost test/cpp { MSFT_Person Key 8 monster }"
+
+    XML_Char data[] = PAL_T("<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://www.w3.org/2003/05/soap-envelope\" ")
+        PAL_T("xmlns:wsa=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" ")
+        PAL_T("xmlns:wsen=\"http://schemas.xmlsoap.org/ws/2004/09/enumeration\" ")
+        PAL_T("xmlns:e=\"http://schemas.xmlsoap.org/ws/2004/08/eventing\" ")
+        PAL_T("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" ")
+        PAL_T("xmlns:wsmb=\"http://schemas.dmtf.org/wbem/wsman/1/cimbinding.xsd\" ") 
+        PAL_T("xmlns:wsman=\"http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd\" ")
+        PAL_T("xmlns:wxf=\"http://schemas.xmlsoap.org/ws/2004/09/transfer\" ")
+        PAL_T("xmlns:cim=\"http://schemas.dmtf.org/wbem/wscim/1/common\" ")
+        PAL_T("xmlns:msftwinrm=\"http://schemas.microsoft.com/wbem/wsman/1/wsman.xsd\" ")
+        PAL_T("xmlns:wsmid=\"http://schemas.dmtf.org/wbem/wsman/identity/1/wsmanidentity.xsd\">\n")
+        PAL_T("<SOAP-ENV:Header>\n")
+        PAL_T("<wsa:To>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</wsa:To>\n")
+        PAL_T("<wsa:Action>http://schemas.dmtf.org/wbem/wsman/1/wsman/fault</wsa:Action>\n")
+        PAL_T("<wsa:MessageID>uuid:B1C29099-3649-0005-0000-000000010000</wsa:MessageID>\n")
+        PAL_T("<wsa:RelatesTo>uuid:B1C28C85-3649-0005-0000-000000010000</wsa:RelatesTo>\n")
+        PAL_T("</SOAP-ENV:Header>\n\n")
+        PAL_T("<SOAP-ENV:Body>\n")
+        PAL_T("<SOAP-ENV:Fault>\n<SOAP-ENV:Code>\n<SOAP-ENV:Value>SOAP-ENV:Receiver</SOAP-ENV:Value>\n")
+        PAL_T("<SOAP-ENV:Subcode>\n<SOAP-ENV:Value>wsman:InternalError</SOAP-ENV:Value>\n</SOAP-ENV:Subcode>\n</SOAP-ENV:Code>\n")
+        PAL_T("<SOAP-ENV:Reason>\n<SOAP-ENV:Text xml:lang=\"en-US\">The specified property does not exist.</SOAP-ENV:Text>\n</SOAP-ENV:Reason>\n")
+        PAL_T("<SOAP-ENV:Detail>\n<p:OMI_Error wsmb:IsCIM_Error=\"true\" ")
+        PAL_T("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" ")
+        PAL_T("xmlns:wsmb=\"http://schemas.dmtf.org/wbem/wsman/1/cimbinding.xsd\" ") 
+        PAL_T("xmlns:p=\"http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/OMI_Error\" ")
+        PAL_T("xmlns:cim=\"http://schemas.dmtf.org/wbem/wscim/1/common\"><p:ErrorType xsi:nil=\"true\"/>\n")
+        PAL_T("<p:OtherErrorType xsi:nil=\"true\"/>\n<p:OwningEntity xsi:type=\"cim:cimString\">OMI:CIMOM</p:OwningEntity>\n")
+        PAL_T("<p:MessageID xsi:type=\"cim:cimString\">OMI:MI_Result:12</p:MessageID>\n")
+        PAL_T("<p:Message xsi:type=\"cim:cimString\">The specified property does not exist.</p:Message>\n")
+        PAL_T("<p:PerceivedSeverity xsi:type=\"cim:cimUnsignedInt\">7</p:PerceivedSeverity>\n")
+        PAL_T("<p:ProbableCause xsi:type=\"cim:cimUnsignedInt\">0</p:ProbableCause>\n")
+        PAL_T("<p:ProbableCauseDescription xsi:type=\"cim:cimString\">Unknown</p:ProbableCauseDescription>\n")
+        PAL_T("<p:RecommendedActions xsi:nil=\"true\"/>\n")
+        PAL_T("<p:ErrorSource xsi:nil=\"true\"/>\n<p:ErrorSourceFormat xsi:nil=\"true\"/>\n<p:OtherErrorSourceFormat xsi:nil=\"true\"/>\n")
+        PAL_T("<p:CIMStatusCode xsi:type=\"cim:cimUnsignedInt\">12</p:CIMStatusCode>\n")
+        PAL_T("<p:CIMStatusCodeDescription xsi:nil=\"true\"/>\n<p:OMI_Code xsi:type=\"cim:cimUnsignedInt\">12</p:OMI_Code>\n")
+        PAL_T("<p:OMI_Category xsi:type=\"cim:cimUnsignedInt\">10</p:OMI_Category>\n")
+        PAL_T("<p:OMI_Type xsi:type=\"cim:cimString\">MI</p:OMI_Type>\n")
+        PAL_T("<p:OMI_ErrorMessage xsi:type=\"cim:cimString\">The specified property does not exist.</p:OMI_ErrorMessage>\n</p:OMI_Error>\n")
+        PAL_T("</SOAP-ENV:Detail>\n</SOAP-ENV:Fault>\n</SOAP-ENV:Body>\n</SOAP-ENV:Envelope>\n");
+
+    memset(&wsheaders, 0, sizeof(wsheaders));
+
+    XML_Init(&xml);
+
+    XML_RegisterNameSpace(&xml, 's',
+                          ZT("http://www.w3.org/2003/05/soap-envelope"));
+    
+    XML_RegisterNameSpace(&xml, 'a',
+                          ZT("http://schemas.xmlsoap.org/ws/2004/08/addressing"));
+
+    XML_RegisterNameSpace(&xml, 'w',
+                          ZT("http://schemas.xmlsoap.org/ws/2005/06/management"));
+
+    XML_SetText(&xml, data);
+
+    if (!NitsCompare(MI_RESULT_OK, WS_ParseSoapEnvelope(&xml), PAL_T("Parse soap envelope error")))
+    {
+        goto cleanup;
+    }
+
+    if (!NitsCompare(MI_RESULT_OK, WS_ParseWSHeader(&xml, &wsheaders, USERAGENT_UNKNOWN), PAL_T("Parse header error")))
+    {
+        goto cleanup;
+    }
+
+    if (!NitsCompare(MI_TRUE, wsheaders.foundAction, PAL_T("No action in header")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompare(wsheaders.rqtAction, WSMANTAG_ACTION_WSMAN_FAULT, PAL_T("Action is not Get Response")))
+    {
+        goto cleanup;
+    }
+
+    if (!NitsCompare(MI_RESULT_OK, WS_ParseFaultBody(&xml, &fault), PAL_T("Unable to retrieve fault")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompareString(fault.code, PAL_T("SOAP-ENV:Receiver"), PAL_T("Mismatch of fault code")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompareString(fault.subcode, PAL_T("wsman:InternalError"), PAL_T("Mismatch of fault subcode")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompareString(fault.reason, PAL_T("The specified property does not exist."), PAL_T("Mismatch of fault subcode")))
+    {
+        goto cleanup;
+    }
+
+    if (!NitsCompare(MI_RESULT_OK, FindErrorCode(&faultCode, &resultCode, wsheaders.rqtAction, "SOAP-ENV:Receiver",
+                                                 "wsman:InternalError", fault.reason), PAL_T("Cannot determine errorCode")))
+    {
+        goto cleanup;
+    }
+    if (!NitsCompare(0, faultCode, PAL_T("Wrong fault code")))        // WSBUF_FAULT_INTERNAL_ERROR
+    {
+        goto cleanup;
+    }
+    if (!NitsCompare(12, resultCode, PAL_T("Wrong result code")))     // MI_RESULT_NO_SUCH_PROPERTY
+    {
+        goto cleanup;
+    }
+    
+
+    cleanup:
+    ;
+}
+NitsEndTest
+
+/* 
+
+"<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:wsa=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" xmlns:wsen=\"http://schemas.xmlsoap.org/ws/2004/09/enumeration\" xmlns:e=\"http://schemas.xmlsoap.org/ws/2004/08/eventing\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:wsmb=\"http://schemas.dmtf.org/wbem/wsman/1/cimbinding.xsd\" xmlns:wsman=\"http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd\" xmlns:wxf=\"http://schemas.xmlsoap.org/ws/2004/09/transfer\" xmlns:cim=\"http://schemas.dmtf.org/wbem/wscim/1/common\" xmlns:msftwinrm=\"http://schemas.microsoft.com/wbem/wsman/1/wsman.xsd\" xmlns:wsmid=\"http://schemas.dmtf.org/wbem/wsman/identity/1/wsmanidentity.xsd\">\n<SOAP-ENV:Header>\n<wsa:To>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</wsa:To>\n<wsa:Action>http://schemas.xmlsoap.org/ws/2004/09/transfer/CreateResponse</wsa:Action>\n<wsa:MessageID>uuid:B1C29099-3649-0005-0000-000000020000</wsa:MessageID>\n<wsa:RelatesTo>uuid:20EC5EEA-364A-0005-0000-000000010000</wsa:RelatesTo>\n</SOAP-ENV:Header><SOAP-ENV:Body><wxf:ResourceCreated>\n<wsa:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</wsa:Address>\n<wsa:ReferenceParameters>\n<wsman:ResourceURI>http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/MSFT_Person</wsman:ResourceURI>\n<wsman:SelectorSet>\n<wsman:Selector Name=\"Key\">8</wsman:Selector></wsman:SelectorSet>\n</wsa:ReferenceParameters>\n</wxf:ResourceCreated>\n</SOAP-ENV:Body></SOAP-ENV:Envelope>"
+
+*/
