e0842222fc015937b422e3b91019fd702524b7a0
George Fleming <v-geflem@microsoft.com>
2016-07-21 11:24:35 -0700

    Update copyright info

--- Unix/wsman/wsmanerrorhandling.h
+++ Unix/wsman/wsmanerrorhandling.h
@@ -1,6 +1,26 @@
-//===============================================================
-// Copyright (c) Microsoft Corporation.  All rights reserved.
-//===============================================================
+/*
+**==============================================================================
+**
+** Open Management Infrastructure (OMI)
+**
+** Copyright (c) Microsoft Corporation
+**
+** Licensed under the Apache License, Version 2.0 (the "License"); you may not
+** use this file except in compliance with the License. You may obtain a copy
+** of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+** KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+** WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+** MERCHANTABLITY OR NON-INFRINGEMENT.
+**
+** See the Apache 2 License for the specific language governing permissions
+** and limitations under the License.
+**
+**==============================================================================
+*/
 
 #pragma once 
 
@@ -260,6 +280,12 @@ typedef struct _SOAP_FAULT_INFORMATION
     MI_Char *SoapSubcode;
     MI_Char *WSManFaultDetail;
     MI_Uint32 SoapFaultActionURI;
+} SOAP_FAULT_INFORMATION;
+
+typedef struct _SOAP_FAULT_INFORMATION
+{
+    ERROR_TYPES errorCode;
+    MI_Result errorResult;
 } SOAP_FAULT_INFORMATION;
 
 BEGIN_EXTERNC
