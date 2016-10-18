02a165abba20fa50b79379c4208522f18ae83fa9
George Fleming <v-geflem@microsoft.com>
2016-10-18 08:57:21 -0700

    Fix Mac seg fault

--- Unix/http/httpclient.c
+++ Unix/http/httpclient.c
@@ -2171,8 +2171,11 @@ Selector *HttpClient_GetSelector(HttpClient *self)
 }
 MI_Result HttpClient_WakeUpSelector(HttpClient *self, MI_Uint64 whenTime)
 {
-    self->connector->base.fireTimeoutAt = whenTime;
-    Selector_Wakeup(self->selector, MI_TRUE );
+    if (self->connector)
+    {
+        self->connector->base.fireTimeoutAt = whenTime;
+        Selector_Wakeup(self->selector, MI_TRUE );
+    }
     return MI_RESULT_OK;
 }
 
