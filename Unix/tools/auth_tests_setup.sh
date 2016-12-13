49196d0f6f721a93a92d16ba46ae6c36959d7757
George Fleming <v-geflem@microsoft.com>
2016-12-13 13:52:19 -0800

    Issue #144:  Determine supported platforms

--- Unix/tools/auth_tests_setup.sh
+++ Unix/tools/auth_tests_setup.sh
@@ -48,3 +48,46 @@ if [ "x${username}" != "x" -a "x${userpasswd}" != "x" ]; then
     NTLM_DOMAIN=$hostname
     export NTLM_DOMAIN
 fi
+
+#
+# As of 12/2016, PowerShell supports following linux platforms only
+# NTLM tests will run on these platforms only
+#
+# Ubuntu 14.04
+# Ubuntu 16.04
+# CentOS 7
+# MacOS 10.11
+
+supported=0
+
+kernel=`uname -s`
+if [ "$kernel" = "Darwin" ]; then
+    echo "Running on a MAC 10.11"
+    supported=1
+elif [ "$kernel" = "Linux" ]; then
+    if [ -f /etc/centos-release ]; then
+        fgrep "release 7" /etc/centos-release > /dev/null
+        if [ $? -eq 0 ]; then
+            echo "Running on CentOS 7"
+            supported=1
+        fi
+    elif [ -f /etc/lsb-release ]; then
+        fgrep "DISTRIB_RELEASE=14.04" /etc/lsb-release > /dev/null
+        if [ $? -eq 0 ]; then
+            echo "Running on Ubuntu 14.04"
+            supported=1
+        else
+            fgrep "DISTRIB_RELEASE=16.04" /etc/lsb-release > /dev/null
+            if [ $? -eq 0 ]; then
+                echo "Running on Ubuntu 16.04"
+                supported=1
+            fi
+        fi
+    else
+        echo "Running on Linux"
+        supported=1
+    fi
+fi
+
+export NTLM_SUPPORTED_PLATFORM
+NTLM_SUPPORTED_PLATFORM=$supported
