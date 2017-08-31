dee95dcf574dc417080053fa780b869e7ce4db9d
Bruce Campbell <v-brucc@microsoft.com>
2017-08-31 15:14:12 -0700

    Add omi.keytab update to the installer

---
+++ Unix/scripts/config_keytab_update.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+#
+# usage: update_keytab.sh
+
+SCRIPTNAME=$0
+OMI_HOME=/opt/omi
+
+syskeytab="/etc/krb5.keytab"
+omikeytab="/etc/opt/omi/creds/omi.keytab"
+ktstrip="$OMI_HOME/bin/support/ktstrip"
+tmpfile=$(mktemp)
+
+checkConfiguration()
+{
+    if ! crontab -l 2> /dev/null | grep -q "$ktstrip" ; then
+        return 1
+    fi
+
+    return 0
+}
+
+
+configure()
+{
+    if checkConfiguration; then
+       timestamp=$(date +%F\ %T)
+       printf $timestamp "System already configured to run $SCRIPTNAME automatically"
+       return 0
+    fi
+
+    # prime the omi keytab
+    $ktstrip $syskeytab $omikeytab
+
+    crontab -l > $tmpfile 2> /dev/null || true
+
+    # We don't worry about log rotate
+    # execute the check every minute.
+
+    echo "* * * * * [ $syskeytab -nt $omikeytab ] && $ktstrip $syskeytab $omikeytab" >>$tmpfile
+
+    crontab $tmpfile
+
+    timestamp=$(date +%F\ %T)
+    printf $timestamp ": Crontab configured to update omi keytab automatically"
+}
+
+unconfigure()
+{
+    if ! checkConfiguration; then
+        timestamp=$(date +%F\ %T)
+        printf $timestamp ": Crontab not configured to update omi keytab automatically. Skip unconfigure"
+        return 0
+    fi
+
+    crontab -l > $tmpfile 2> /dev/null || true
+
+    egrep -v "$omikeytab" $tmpfile | crontab
+
+    timestamp=$(date +%F\ %T)
+    printf $timestamp ": Crontab no longer configured to update omi keytab."
+}
+
+
+
+while [ $# -ne 0 ]
+do
+    case "$1" in
+	--configure)
+        configure
+	    shift 1
+	    ;;
+
+	--unconfigure)
+        unconfigure
+	    shift 1
+	    ;;
+
+    *)
+        printf "$0 unknown option"
+        numargs=0
+        ;;
+    esac
+done
+
