b9cb191d117ac5fff182e58d214a9076ae5203c5
John Liu <bmzyDream@gmail.com>
2018-11-22 21:39:10 -0800

    SDL items assigned to John

--- Unix/sock/sock.c
+++ Unix/sock/sock.c
@@ -54,6 +54,7 @@
 #endif
 
 #define MINSOCKBUFSIZE 4096
+#define MAXSOCKBUFSIZE 524288
 
 /*
 **==============================================================================
@@ -113,12 +114,25 @@ MI_INLINE int _Write(Sock sock, const void* data, size_t size)
 
 MI_INLINE int _ReadV(Sock sock, const IOVec* iov, size_t  iovcnt)
 {
-    return readv(sock, (struct iovec*)iov, iovcnt);
+    int n = readv(sock, (struct iovec*)iov, iovcnt);
+    if (n < 0)
+    {
+        if (errno == EAGAIN)
+            LOGD2((ZT("_ReadV - read failed with EAGAIN. socket: %d"), sock));
+        else
+            LOGE2((ZT("_ReadV - read failed. socket: %d, errno: %d (%s)"), sock, errno, strerror(errno)));
+    }
+    return n;
 }
 
 MI_INLINE int _WriteV(Sock sock, const IOVec* iov, size_t  iovcnt)
 {
-    return writev(sock, (struct iovec*)iov, iovcnt);
+    int n = writev(sock, (struct iovec*)iov, iovcnt);
+    if (n < 0)
+    {
+        LOGE2((ZT("_WriteV - write failed. socket: %d, errno: %d (%s)"), sock, errno, strerror(errno)));
+    }
+    return n;
 }
 
 /*
@@ -340,6 +354,12 @@ MI_Result Sock_Read(
     LOGD2((ZT("Sock_Read - Begin. socket: %d, size: %u"), (int)self, (unsigned int)size));
 
     *sizeRead = 0;
+    
+    if(size > MAXSOCKBUFSIZE)
+    {
+        LOGD2((ZT("Sock_Read - failed: To large %d bytes read, the preset max size is %d bytes"), size, MAXSOCKBUFSIZE));
+        return MI_RESULT_FAILED;
+    }
 
     for ( ; ; )
     {
@@ -388,6 +408,12 @@ MI_Result Sock_Write(
     LOGD2((ZT("Sock_Write - Begin. socket: %d, size: %u"), (int)self, (unsigned int)size));
 
     *sizeWritten = 0;
+    
+    if(size > MAXSOCKBUFSIZE)
+    {
+        LOGD2((ZT("Sock_Write - failed: To large %d bytes written, the preset max size is %d bytes"), size, MAXSOCKBUFSIZE));
+        return MI_RESULT_FAILED;
+    }
 
     for ( ; ; )
     {
@@ -441,6 +467,12 @@ MI_Result Sock_ReadV(
     int n;
 
     * sizeRead = 0;
+    
+    if(iovcnt > MAXSOCKBUFSIZE)
+    {
+        LOGD2((ZT("Sock_ReadV - failed: To large %d bytes read, the preset max size is %d bytes"), iovcnt, MAXSOCKBUFSIZE));
+        return MI_RESULT_FAILED;
+    }
 
     for ( ; ; )
     {
@@ -448,7 +480,7 @@ MI_Result Sock_ReadV(
         n = _ReadV(self, iov, iovcnt);
 
         if (n >= 0)
-        {
+        {           
             *sizeRead = n;
             return MI_RESULT_OK;
         }
@@ -478,6 +510,12 @@ MI_Result Sock_WriteV(
     int n, error;
 
     * sizeWritten = 0;
+    
+    if(iovcnt > MAXSOCKBUFSIZE)
+    {
+        LOGD2((ZT("Sock_WriteV - failed: To large %d bytes written, the preset max size is %d bytes"), iovcnt, MAXSOCKBUFSIZE));
+        return MI_RESULT_FAILED;
+    }
 
     for ( ; ; )
     {
@@ -485,7 +523,7 @@ MI_Result Sock_WriteV(
         n = _WriteV(self, iov, iovcnt);
 
         if (n >= 0)
-        {
+        {            
             *sizeWritten = n;
             return MI_RESULT_OK;
         }
@@ -728,12 +766,12 @@ MI_Result Sock_CreateIPConnector(
     if (len >= sizeof host)
         return MI_RESULT_INVALID_PARAMETER;
     port = (unsigned short)atol(posColon + 1);
-	memcpy(host, hostAndPort, len);
+    memcpy(host, hostAndPort, len);
     host[len] = '\0';
 
     /* This code tries to connect using the primary addressing family */
-		/* (IPv4 or IPv6). If that fails and Addr_Init has a secondary */
-		/* addressing family, it tries using the secondary family next. */
+        /* (IPv4 or IPv6). If that fails and Addr_Init has a secondary */
+        /* addressing family, it tries using the secondary family next. */
 
     /* Initialize primary family address. */
     r = Addr_Init(&addr, host, port, MI_FALSE);
